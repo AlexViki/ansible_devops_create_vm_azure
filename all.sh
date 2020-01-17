e28604a16dd1e63da0c50b056f9ad9c719647a5d
Alex Viki <shevashokc@gmail.com>
2020-01-17 07:45:51 +0000

    add new commands to file all.sh

--- all.sh
+++ all.sh
@@ -58,7 +58,7 @@ echo "###########################################################"
 echo "Clone playbook file from GIT"
 echo "###########################################################"
 
-#git clone
+git clone https://github.com/AlexViki/ansible_devops_create_vm_azure_yml_file.git create_vm.yml
 
 echo "###########################################################"
 echo "RUN playbook "
